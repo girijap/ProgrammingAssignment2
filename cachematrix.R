62f3361a8562fad34185705eb7f1d3df3712e858
girijap <gparmeshwar@gmail.com>
2014-10-25 00:08:21 -0700

    updates for Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions show the effects of lexical scoping on variables 
+## by initializing a matrix and caching the inverse of a matrix using the
+## super assignment operator <<- in the makeCacheMatrix() function. 
+## The inverse of this matrix is computed by using the R solve() function in the
+## cacheSolve() function below.
 
-## Write a short comment describing this function
+## This function declares and initializes matrix m
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of an invertible matrix using solve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
